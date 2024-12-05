f005965b797b7a887662d80928a40632f1c4efa7
yuma0313 <git@stackblitz.com>
2024-12-05 15:44:14 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useRef, useEffect } from 'react';
+import { Box } from "@chakra-ui/react";
+
+function App() {
+  const videoEditorRef = useRef<any>();
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  async function init() {
+    if (!videoEditorRef.current) {
+      return;
+    }
+
+    videoEditorRef.current.addEventListener("onReady", handleReady);
+  }
+
+  async function handleReady() {
+    await videoEditorRef.current!.getEngine();
+  }
+
+  return (
+    <Box w="full" h="full">
+      {/* @ts-ignore */}
+      <rendley-video-editor
+        ref={videoEditorRef}
+        licensename="test-revot"
+        licensekey="C28B6A4E8F75F26929420001"
+        theme="dark"
+      />
+    </Box>
+  );
+}
+
+export default App;
